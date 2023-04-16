e6aa244060c635e6483aa4fa8637098d23cb97a2
nicety <plnicetyde@gmail.com>
2023-04-16 18:06:20 +0200

    fixes, changes, very nice site.
    Co-authored-by: immpersion <immpersion@users.noreply.github.com>

--- arro.gg/src/app/app.module.ts
+++ arro.gg/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent
+    HeroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
