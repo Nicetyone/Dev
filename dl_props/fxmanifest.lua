47059072df243fa039a04728a5f8fa41a3753e7e
Nicety <78967461+Nicetyone@users.noreply.github.com>
2023-09-25 19:52:56 +0200

    Initalization of dl_props

---
+++ dl_props/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'bruno63'
+description 'DreamLife Props'
+
+files {
+	'destiny_prop.ytyp',
+	--'police_props.ytyp',
+	--'propy_jedzenia.ytyp',
+	--'bruno_xmas.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'destiny_prop.ytyp'
+--data_file 'DLC_ITYP_REQUEST' 'police_props.ytyp'
+--data_file 'DLC_ITYP_REQUEST' 'propy_jedzenia.ytyp'
+--data_file 'DLC_ITYP_REQUEST' 'bruno_xmas.ytyp'
